ae4d5890b4eb7578a3f95a6cc03f4da588ee6188
jspashalon <jparker@andrews.edu>
2025-02-03 12:22:26 -0500

    in-class-assingment-05

--- tests/test_my_math.py
+++ tests/test_my_math.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.my_math import sum, multiply, difference
+from src.my_math import sum, multiply, difference, absolute_sum, calculate_birth_year
 
 def test_sum():
     res = sum(1,1)
@@ -21,8 +21,19 @@ def test_difference():
 
 # Work together
 ## Test absolute sum
+def test_it_return_positive_numbers():
+    res = absolute_sum(5, -10)
+    assert res == 15
+    
+def test_it_should_return_postive():
+    res = absolute_sum(-5, 10)
+    assert res == 15
+
 
 
 
 # Check for understanding
 ## Test calculate age
+def test_calculate_age():
+    res = calculate_birth_year(2025, 25, True)
+    assert res == 2000
